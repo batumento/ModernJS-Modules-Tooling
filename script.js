6d4116f4ceefb181f1e48262535fb0b3f06958b8
Batuhan Emiroğlu <74363208+batumento@users.noreply.github.com>
2023-12-28 12:15:50 +0300

    Introduction NPM

--- script.js
+++ script.js
@@ -21,44 +21,61 @@ console.log(cart);
 //console.log('Something');
 
 //Top-level Await kullanım faydaları
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
-  console.log(data);
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
-
-const lastPost = await getLastPost();
-console.log(lastPost);
-
+//const getLastPost = async function () {
+//  const res = await fetch('https://jsonplaceholder.typicode.com///posts');
+//  const data = await res.json();
+//  console.log(data);
+//  return { title: data.at(-1).title, text: data.at(-1).body };
+//};
+//
+//const lastPost = await getLastPost();
+//console.log(lastPost);
+//
 //Module Pattern
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+//const ShoppingCart2 = (function () {
+//  const cart = [];
+//  const shippingCost = 10;
+//  const totalPrice = 237;
+//  const totalQuantity = 23;
+//
+//  const addToCard = function (product, quantity) {
+//    cart.push({ product, quantity });
+//    console.log(`${quantity} ${product} added to cart`);
+//  };
+//
+//  const orderStock = function (product, quantity) {
+//    cart.push({ product, quantity });
+//    console.log(`${quantity} ${product} ordered from supplier`);
+//  };
+//
+//  return {
+//    addToCard,
+//    cart,
+//    totalPrice,
+//    totalQuantity,
+//  };
+//})();
+//
+//ShoppingCart2.addToCard('Elmac', 15);
+//ShoppingCart2.addToCard('Plati', 45);
+//ShoppingCart2.addToCard('Altind', 25);
+//console.log(ShoppingCart2);
+//console.log(ShoppingCart2.shippingCost);
+//
 
-  const addToCard = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
-  const orderStock = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
 
-  return {
-    addToCard,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
-
-ShoppingCart2.addToCard('Elmac', 15);
-ShoppingCart2.addToCard('Plati', 45);
-ShoppingCart2.addToCard('Altind', 25);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
+const stateClone = Object.assign({}, state);
+const stateCloneDeep = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateCloneDeep);
